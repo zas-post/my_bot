357cb81b6ecc7856902b82b738be9a58a85a876b
Alexander <zas.post@gmail.com>
2024-10-09 14:13:16 +0300

    .gitignore is now working

--- test_config.py
+++
@@ -1,8 +0,0 @@
-# test_config.py
-from config_data.config import load_config
-
-if __name__ == "__main__":
-    config = load_config(".env")
-    print("Bot Token:", config.tg_bot.token)
-    print("Admin IDs:", config.tg_bot.admin_id)
-    print("Database File:", config.db.db_file)
